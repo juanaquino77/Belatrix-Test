a7cfd2c24445740645fe941a49c328db3882b682
juanaquino77 <juanaquino77@gmail.com>
2018-01-05 19:37:27 -0300

    Name Fixing
    Fix de Nombres y variables

--- Belatrix/src/app/app.component.ts
+++ Belatrix/src/app/app.component.ts
@@ -1,25 +1,22 @@
 import { Pipe,PipeTransform, Component, OnInit } from '@angular/core';
 
 import {FileService} from './app.service';
-import {Localidad} from './localidad/localidad.component';
+import {Location} from './location/location.component';
 import { PapaParseService } from 'ngx-papaparse';
 //import {Pipe, PipeTransform} from 'angular/core';
 import {Place} from './place/place.component';
-import {LocalidadService} from './services/localidad.service';
+import {LocationService} from './services/location.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-  providers:[LocalidadService, Localidad]
+  styleUrls: ['./app.component.css'],
+  providers:[LocationService, Location]
 })
 
-export class AppComponent implements OnInit{ 
+export class AppComponent { 
  
   constructor(private fileService: FileService, private papa: PapaParseService){  
   );
-            
-  private ngOnInit(): void{
-  };
 }
          
